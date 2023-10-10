d822d05d6ec5988fe2c86600722f74cd1d37917a
vi66r <rafi.rizwan.616@gmail.com>
2023-10-10 14:09:40 -0400

    Update Components/Components/ContentView.swift

--- Components/Components/ContentView.swift
+++ Components/Components/ContentView.swift
@@ -4,88 +4,117 @@ struct ContentView: View {
     var body: some View {
         TabView {
             NavigationView {
-                FeedView()
+                HomeView()
             }
             .tabItem {
                 Image(systemName: "house.fill")
                 Text("Home")
             }
             
+            NavigationView {
+                SearchView()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+            
+            NavigationView {
+                TripsView()
+            }
+            .tabItem {
+                Image(systemName: "airplane")
+                Text("Trips")
+            }
+            
+            NavigationView {
+                ProfileView()
+            }
+            .tabItem {
+                Image(systemName: "person.fill")
+                Text("Profile")
+            }
+        }
+        .accentColor(.red)
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+            .navigationBarTitle("Airbnb", displayMode: .inline)
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            AirbnbSearchBar()
             Text("Search")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
-                }
-            
-            Text("New Post")
-                .tabItem {
-                    Image(systemName: "plus.square")
-                    Text("New Post")
-                }
-            
-            Text("Activity")
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("Activity")
-                }
-            
-            Text("Profile")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Profile")
-                }
+                .navigationBarTitle("Search", displayMode: .inline)
         }
     }
 }
 
-struct FeedView: View {
+struct TripsView: View {
     var body: some View {
-        List {
-            ForEach(0..<10) { _ in
-                PostView()
+        Text("Trips")
+            .navigationBarTitle("Trips", displayMode: .inline)
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+            .navigationBarTitle("Profile", displayMode: .inline)
+    }
+}
+
+struct AirbnbSearchBar: View {
+    var body: some View {
+        Button(action: {}) {
+            HStack(spacing: 15) {
+                Image(systemName: "magnifyingglass")
+                    .font(.system(size: 18))
+                    .padding(.vertical, 12)
+                    .foregroundColor(.primary)
+                    .padding(.leading, 6.0)
+                VStack(alignment: .leading, spacing: 3) {
+                    Text("Where to?")
+                        .foregroundColor(.black)
+                        .fontWeight(.medium)
+                        .font(.system(size: 14))
+
+                    Text("Anywhere • Any week • Add guests")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .fontWeight(.regular)
+                        .font(.system(size: 8))
+                        .lineLimit(1)
+                }
+                .padding(.trailing)
             }
-        }
-        .navigationBarTitle("Instagram", displayMode: .inline)
-    }
-}
-
-struct PostView: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack {
-                Image(systemName: "person.circle")
-                    .resizable()
+            .padding(.vertical, 6.0)
+            Button(action: {}) {
+                Image(systemName: "slider.horizontal.3")
+                    .font(.headline)
                     .frame(width: 40, height: 40)
-                Text("Username")
-                Spacer()
-                Image(systemName: "ellipsis")
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    .foregroundColor(.black)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 50)
+                            .stroke(Color(.systemGray6), lineWidth: 2)
+                    )
             }
-            .padding(.horizontal)
-            
-            Image(systemName: "photo")
-                .resizable()
-                .scaledToFit()
-            
-            HStack(spacing: 16) {
-                Image(systemName: "heart")
-                Image(systemName: "bubble.right")
-                Image(systemName: "paperplane")
-                Spacer()
-                Image(systemName: "bookmark")
-            }
-            .padding(.horizontal)
-            .font(.system(size: 24))
-            
-            Text("100 likes")
-                .padding(.horizontal)
-            
-            HStack {
-                Text("Username")
-                    .bold()
-                Text("Caption")
-            }
-            .padding(.horizontal)
+            .padding(.leading, 0.0)
         }
+        .padding(.horizontal, 8.0)
+        .background(
+            RoundedRectangle(cornerRadius: 40)
+                .fill(Color.white)
+                .shadow(color: .gray.opacity(0.2), radius: 5, x: 0, y: 0)
+        )
     }
 }
 
